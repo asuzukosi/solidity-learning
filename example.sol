04426876d165fe6694560b778fe44c360f265a9f
eHealth4everyone <kosisochukwu.asuzu@e4email.net>
2022-02-21 23:29:20 +0100

    feat: made new changes

--- example.sol
+++ example.sol
@@ -3,6 +3,20 @@ contract SolidityTest {
    constructor() public{
    }
    function getResult() public view returns(uint){
+       // this is a comment similar to c++
+       /*
+            This is a mutli
+                      line
+                      comment
+            also similar to c++
+        */
+
+    
+    //   address x = 0x212;
+    //   address myAddress = this;
+    //   if (x.balance < 10 && myAddress.balance >= 10) x.transfer(10);
+
+
       uint a = 1; // defining a vairable of type uint
       uint b = 2; 
       uint result = a + b; // storing result of operation in a variable result
